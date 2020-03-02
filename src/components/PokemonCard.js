9c8cfa2795042f79a1b6717db60f022856b41969
eliastooloee <elitooloee@gmail.com>
2020-03-02 13:48:02 -0800

    fixed db

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -24,7 +24,7 @@ const PokemonCard = props => {
                     {/* add types and pokeball if collected */}
                 </div>
             </div>
-        </div >
+        </div>
     );
 
 };
