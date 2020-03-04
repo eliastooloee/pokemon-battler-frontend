d311f4104c6a572b09cf430b73e6f829ff924e07
eliastooloee <elitooloee@gmail.com>
2020-03-04 14:46:04 -0800

    added win/loss indicators to battle

---
+++ src/components/Battle.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PokemonCard from "../components/PokemonCard";
+import FightCard from "../components/FightCard";
+
+
+class Battle extends React.Component {
+
+    battleResults = (pokemon) => {
+    
+        return this.props.winner===pokemon 
+       
+    }
+
+    
+    render() {
+        return (
+          <div className="battle">
+            <FightCard  battleResults={this.battleResults(this.props.currentPokemon)} pokemon= {this.props.currentPokemon}/>
+            <button type="button" onClick={() =>
+                this.props.battle()}>Fight!</button>
+            <FightCard battleResults={this.battleResults(this.props.enemyPokemon)} pokemon = {this.props.enemyPokemon} /> 
+          </div>
+        );
+      }
+}
+export default Battle;
---
+++ src/components/FightCard.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+const FightCard = props => {
+    const { pokemon, victoryStyle } = props;
+
+
+    function getPokemonNum(num) {
+        return ('00' + num).slice(-3)
+    }
+
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1)
+    }
+
+    return (
+        <div className="ui column">
+            <div className="fight-card">
+                <div id="winner" style={victoryStyle}> {props.battleResults? 'Winner!': 'Loser'} </div>
+                <div className="image">
+                    <img alt="pokemon sprite" src={pokemon.sprite_front} />
+                </div>
+                <div className="content">
+                    <div className="header">
+                        <p>#{getPokemonNum(pokemon.id)}</p>
+                        <h3>{capitalizeFirstLetter(pokemon.species)}</h3>
+                        <p>Hit Points: {pokemon.stat_hp}</p>
+                        <p> Speed: {pokemon.stat_speed}</p>
+                        <p>Attack:{pokemon.stat_attack}</p>
+                        <p>Special Attack: {pokemon.stat_special_attack}</p>
+                        <p>Defense: {pokemon.stat_defense}</p>
+                        <p>Special Defense: {pokemon.stat_special_defense}</p>
+                    </div>
+                    {/* add types and pokeball if collected */}
+                </div>
+            </div>
+        </div>
+    );
+
+};
+
+export default FightCard;
