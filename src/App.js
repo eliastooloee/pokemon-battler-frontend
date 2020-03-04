812a02ea96bfd3141e2cb356748cf0a0fc516110
eliastooloee <elitooloee@gmail.com>
2020-03-03 16:18:31 -0800

    added login and bootstrap

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import PokemonPage from "./containers/PokemonPage";
 import NavBar from './components/NavBar'
-
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class App extends Component {
