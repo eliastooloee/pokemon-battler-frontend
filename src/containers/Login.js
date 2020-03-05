e55f5cddb4926b4429468b8de6d75c554630fd03
christinamcmahon <christina.mcmahon1@gmail.com>
2020-03-05 11:09:41 -0800

    adjust login location

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
@@ -38,7 +37,7 @@ export default class Login extends React.Component {
                     direction="column"
                     alignItems="center"
                     justify="center"
-                    style={{ margin: '10vh' }}>
+                    style={{ marginTop: '15vh' }}>
                     <Card variant="outlined">
                         <CardContent>
                             <form onSubmit={this.handleSubmit}>
